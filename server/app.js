0840ce63ac59dc573727310e8c8fe7a810499f28
saba871 <sabatabatadze40@gmail.com>
2025-11-30 03:20:00 -0800

    added dist folder

--- server/app.js
+++ server/app.js
@@ -1,51 +1,50 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const path = require("path");
-const cookieParser = require("cookie-parser"); //
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+const cookieParser = require('cookie-parser'); //
 
-const postRouter = require("./router/post.router");
-const globalErrorHandle = require("./controllers/error.coontroller");
-const authContext = require("./router/auth.router");
+const postRouter = require('./router/post.router');
+const globalErrorHandle = require('./controllers/error.coontroller');
+const authContext = require('./router/auth.router');
 
 dotenv.config();
 const app = express();
 
-app.use(
-  cors({
+app.use(cors({
     origin: process.env.CLIENT_URL,
-    // origin: 'http://localhost:5173',
     credentials: true,
-  })
-);
+}));
+
+// origin: 'http://localhost:5173',
 // process.env.CLIENT_URL
 
 app.use(express.json());
 app.use(cookieParser());
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
 }
 
-app.use("/api/posts", postRouter);
-app.use("/api/auth", authContext);
+app.use('/api/posts', postRouter);
+app.use('/api/auth', authContext);
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // global error handling
 app.use(globalErrorHandle);
 
 mongoose
-  .connect(process.env.DATABASE_URL)
-  .then(() => {
-    console.log("Connected to mongoDB");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
+    .connect(process.env.DATABASE_URL)
+    .then(() => {
+        console.log('Connected to mongoDB');
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(`DataBase Connecting error: ${err}`);
+        process.exit(1);
     });
-  })
-  .catch((err) => {
-    console.log(`DataBase Connecting error: ${err}`);
-    process.exit(1);
-  });
